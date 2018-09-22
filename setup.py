bb6d426025c7c1c0cb7f0d048e5c97974f19359a
Trader Drei <scarletmachine@gmail.com>
2018-09-22 22:27:12 +0200

    Bumping the version + fixing third fridays

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     author='Boyan Soubachov',
     author_email='boyanvs@gmail.com',
     url='http://pypi.python.org/pypi/tastyworks/',
-    version='3.0.1',
+    version='3.1.0',
     packages=find_packages(exclude=['main.py']),
     python_requires='>= 3.6.0',
     description='Tastyworks (unofficial) API',
