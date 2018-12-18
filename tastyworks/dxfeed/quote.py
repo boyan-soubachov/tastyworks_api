a089ffd0b3843c97306284faf0997e8a19206a49
Jonathon J. Howey <jjhowey@gmail.com>
2018-12-18 01:27:00 -0600

    FIx for missing dict keys + personal forrmatting preference

--- tastyworks/dxfeed/quote.py
+++ tastyworks/dxfeed/quote.py
@@ -7,9 +7,11 @@ class Quote(MappedItem):
 
     def _process_fields(self, data_dict: dict):
         for key in ('askTime', 'bidTime', 'time'):
-            data_dict[key] = datetime.datetime.fromtimestamp(data_dict[key] / 1000)
+            if key in data_dict:
+                data_dict[key] = str(datetime.datetime.fromtimestamp(data_dict[key] / 1000))
 
-        data_dict['timeNanos'] = datetime.datetime.fromtimestamp(data_dict['timeNanos'] / 1000_000_000)
+        if 'timeNanos' in data_dict:
+            data_dict['timeNanos'] = str(datetime.datetime.fromtimestamp(data_dict['timeNanos'] / 1000_000_000))
         return data_dict
 
     def __init__(self, data=None):
