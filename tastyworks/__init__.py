eddbe7fbdcd00ae86fb3d33124630a86fb72f8b2
Boyan <boyanvs@gmail.com>
2019-02-19 22:59:55 +0200

    WIP

--- tastyworks/__init__.py
+++ tastyworks/__init__.py
@@ -5,11 +5,11 @@ log = logging.getLogger(__name__)
 log.propagate = False
 out_hdlr = logging.StreamHandler(sys.stdout)
 out_hdlr.setFormatter(logging.Formatter('%(asctime)s %(message)s'))
-out_hdlr.setLevel(logging.INFO)
+out_hdlr.setLevel(logging.DEBUG)
 log.addHandler(out_hdlr)
-log.setLevel(logging.INFO)
+log.setLevel(logging.DEBUG)
 
 root = logging.getLogger()
 root.addHandler(out_hdlr)
 root.propagate = False
-root.setLevel(logging.INFO)
+root.setLevel(logging.DEBUG)
