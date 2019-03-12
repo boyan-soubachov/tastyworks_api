9934fbca77ff9b054eaf3f53d72dd87942bf8649
Josh S <joshs85@gmail.com>
2019-03-12 00:48:09 -0400

    Added alerts get and set

--- tastyworks/models/trading_account.py
+++ tastyworks/models/trading_account.py
@@ -130,7 +130,7 @@ class TradingAccount(object):
             data = alert_from_dict((await response.json())['data']['items'])
         return data
 
-    async def set_quote_alert(self, session, alert: Alert):
+    async def set_quote_alert(session, alert: Alert):
         """
         Create a quote alert.
 
