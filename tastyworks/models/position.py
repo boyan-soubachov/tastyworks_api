719b1aa3f070902b435f4f807fc9b98a0719404e
Josh S <joshs85@gmail.com>
2019-03-12 22:42:00 -0400

    Added abillity to get a closing order object from a position

--- tastyworks/models/position.py
+++ tastyworks/models/position.py
@@ -1,6 +1,10 @@
 from datetime import date, datetime
 from decimal import Decimal
 from tastyworks.models.alert import Alert, AlertField, Operator
+from tastyworks.models.order import (Order, OrderDetails, OrderPriceEffect, OrderType)
+from tastyworks.models.option import Option, OptionType
+from tastyworks.models.underlying import UnderlyingType
+
 import string
 from enum import Enum
 
@@ -44,6 +48,32 @@ class Position(object):
     created_at: datetime = None
     updated_at: datetime = None
 
+    def is_option(self):
+        if self.instrument_type == InstrumentType.EQUITY_OPTION:
+            return True
+        else:
+            return False
+
+    def get_option_obj(self):
+        exp_date = datetime.strptime(self.symbol[6:12], '%y%m%d').date()
+        option_type = OptionType(self.symbol[12:13])
+        strike = Decimal(self.symbol[13:])/1000
+        return Option(ticker=self.underlying_symbol, quantity=self.quantity, expiry=exp_date, strike=strike, option_type=option_type, underlying_type=UnderlyingType.EQUITY)
+
+    def get_closing_order_price_effect(self):
+        if self.cost_effect == PositionCostEffect.CREDIT:
+            return OrderPriceEffect.DEBIT
+        elif self.cost_effect == PositionCostEffect.DEBIT:
+            return OrderPriceEffect.CREDIT
+
+    def get_closing_order_object(self, price: Decimal = self.mark_price, order_type: OrderType = OrderType.LIMIT):
+        closing_order_price_effect = self.get_closing_order_price_effect()
+        details = OrderDetails(type=order_type, price=price, price_effect=closing_order_price_effect)
+        new_order = Order(details)
+        opt = self.get_option_obj()
+        new_order.add_leg(opt)
+        return new_order
+
     def get_last_stock_price_alert_oobject(self, Price: Decimal):
         return Alert(alert_field=AlertField('Last'),operator=self.get_alert_operator(),threshold=Price,symbol=self.underlying_symbol)
 
@@ -57,7 +87,7 @@ class Position(object):
             if self.cost_effect == PositionCostEffect.CREDIT:       # Buy
                 return Operator.GREATERTHAN
             elif self.cost_effect == PositionCostEffect.DEBIT:      # Sell
-                return Operator.LESSTHAN    
+                return Operator.LESSTHAN
 
     @classmethod
     def from_dict(cls, input_dict: dict):
