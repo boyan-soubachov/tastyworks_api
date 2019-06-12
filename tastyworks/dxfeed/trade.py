b5974c2fe13da2489da3c06e9a98a2026c44a0e6
bad-trader09 <51728101+bad-trader09@users.noreply.github.com>
2019-06-11 22:28:11 -0500

    Change time conversion field from eventTime to time. eventTime for trade is always 0, time records the actual time the trade took place.

--- tastyworks/dxfeed/trade.py
+++ tastyworks/dxfeed/trade.py
@@ -6,7 +6,7 @@ class Trade(MappedItem):
     DXFEED_TEXT = 'Trade'
 
     def _process_fields(self, data_dict: dict):
-        data_dict['eventTime'] = datetime.datetime.fromtimestamp(data_dict['eventTime'] / 1000_000_000)
+        data_dict['time'] = datetime.datetime.fromtimestamp(data_dict['time'] / 1000_000_000)
         return data_dict
 
     def __init__(self, data=None):
